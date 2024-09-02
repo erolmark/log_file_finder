dfdd32d06b7831f191ee4645a53b2584664b951e
erol <165566710+erolmark@users.noreply.github.com>
2024-09-03 00:30:13 +0300

    Update log_bulucu.py

--- log_bulucu.py
+++ log_bulucu.py
@@ -1,38 +1,38 @@
-from urllib2 import Request, urlopen, URLError, HTTPError
+from urllib.request import Request, urlopen, URLError, HTTPError
 
 def Space(j):
-	i = 0
-	while i<=j:
-		print " ",
-		i+=1
-
+    i = 0
+    while i <= j:
+        print(" ", end='')
+        i += 1
+    print()  # Satır sonu
 
 def findAdmin():
-	f = open("link.txt","r");
-	link = raw_input("Site İsmini Gir \n(örnek : example.com or www.example.com ): ")
-	print "\n\n Mevcut Linkler: \n"
-	while True:
-		sub_link = f.readline()
-		if not sub_link:
-			break
-		req_link = "http://"+link+"/"+sub_link
-		req = Request(req_link)
-		try:
-			response = urlopen(req)
-		except HTTPError as e:
-			continue
-		except URLError as e:
-			continue
-		else:
-			print "Basarili Log Dosyası=> ",req_link
+    with open("link.txt", "r") as f:
+        link = input("Site İsmini Gir \n(örnek : example.com or www.example.com ): ")
+        print("\n\n Mevcut Linkler: \n")
+        while True:
+            sub_link = f.readline().strip()
+            if not sub_link:
+                break
+            req_link = f"http://{link}/{sub_link}"
+            req = Request(req_link)
+            try:
+                response = urlopen(req)
+            except HTTPError as e:
+                continue
+            except URLError as e:
+                continue
+            else:
+                print("Başarılı Log Dosyası=> ", req_link)
 
 def Credit():
-	Space(9); print "#####################################"
-	Space(9); print "#   +++ Log Dosyası Bulucu +++   #"
-	Space(9); print "#     Developers Veraildez&Medusa   #"
-	Space(9); print "#    Versa Community Versa Team   #"
-	Space(9); print "#####################################"
-	print("daha yapım aşamasındadır. siteyi yazarken sonuna / koymayin!")
+    Space(9); print("#####################################")
+    Space(9); print("#   +++ Log Dosyası Bulucu +++   #")
+    Space(9); print("#     Developers Veraildez&Medusa   #")
+    Space(9); print("#    Versa Community Versa Team   #")
+    Space(9); print("#####################################")
+    print("Daha yapım aşamasındadır. Siteyi yazarken sonuna / koymayın!")
 
 Credit()
 findAdmin()
